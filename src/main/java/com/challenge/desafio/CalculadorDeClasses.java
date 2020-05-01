9a3d1d0a0461aae6c5a79794c4cb8b372c206e2c
Montival Junior <montival_junior@yahoo.com.br>
2020-05-01 12:34:33 -0300

    Commit inicial desafio aceleraDev Java

---
+++ src/main/java/com/challenge/desafio/CalculadorDeClasses.java
@@ -0,0 +1,26 @@
+package com.challenge.desafio;
+
+import com.challenge.interfaces.Calculavel;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class CalculadorDeClasses implements Calculavel {
+
+    @Override
+    public BigDecimal somar(Object object) {
+        return null;
+    }
+
+    @Override
+    public BigDecimal subtrair(Object object) {
+        return null;
+    }
+
+    @Override
+    public BigDecimal totalizar(Object object) {
+        return null;
+    }
+
+
+}
