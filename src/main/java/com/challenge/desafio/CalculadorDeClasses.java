269e3c16e000d081b4ad3b989334a3fbf989688f
Montival Junior <montival_junior@yahoo.com.br>
2020-05-02 22:15:34 -0300

    Commit resolução desafio aceleraDev Java

--- src/main/java/com/challenge/desafio/CalculadorDeClasses.java
+++ src/main/java/com/challenge/desafio/CalculadorDeClasses.java
@@ -1,83 +1,52 @@
 package com.challenge.desafio;
 
-
 import com.challenge.annotation.Somar;
 import com.challenge.annotation.Subtrair;
 import com.challenge.interfaces.Calculavel;
 
-
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.math.BigDecimal;
-import java.util.Arrays;
 
 
 public class CalculadorDeClasses implements Calculavel {
 
     @Override
     public BigDecimal somar(Object object) {
-        Class objeto = Object.class; //Peguei a Classe
-        Field[] fields = objeto.getFields(); // Peguei os atributos
-        BigDecimal[] fieldSomar = new BigDecimal[fields.length];
-        Object myObject = new Object();
-        BigDecimal bigDecimal = BigDecimal.ZERO;
 
-        int i = 0;
-        for (Field field : fields) {
-            i++;
-            if (!field.isAnnotationPresent(Somar.class) && !field.isAnnotationPresent(Somar.class)) {
-                return BigDecimal.ZERO;
-            }
-            if (field.isAnnotationPresent(Somar.class)) {
-                if (field.getType() != BigDecimal.class) {
-                    return BigDecimal.ZERO;
-                } else {
-                    try {
-                        fieldSomar[i] = (BigDecimal) field.get(myObject);
-                        bigDecimal.add(fieldSomar[i]);
-                    } catch (IllegalAccessException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
-        return bigDecimal;
+        return realizarOperacao(object, Somar.class);
     }
 
     @Override
     public BigDecimal subtrair(Object object) {
-        Class objeto = Object.class; //Peguei a Classe
-        Field[] fields = objeto.getFields(); // Peguei os atributos
-        BigDecimal[] fieldSomar = new BigDecimal[fields.length];
-        Object myObject = new Object();
-        BigDecimal bigDecimal = BigDecimal.ZERO;
 
-        int i = 0;
-        for (Field field : fields) {
-            i++;
-            if (!field.isAnnotationPresent(Subtrair.class) && !field.isAnnotationPresent(Subtrair.class)) {
-                return BigDecimal.ZERO;
-            }
-            if (field.isAnnotationPresent(Subtrair.class)) {
-                if (field.getType() != BigDecimal.class) {
-                    return BigDecimal.ZERO;
-                } else {
-                    try {
-                        fieldSomar[i] = (BigDecimal) field.get(myObject);
-                        bigDecimal.add(fieldSomar[i]);
-                    } catch (IllegalAccessException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
-        return bigDecimal;
-
+        return realizarOperacao(object, Subtrair.class);
     }
 
     @Override
     public BigDecimal totalizar(Object object) {
+
         return somar(object).subtract(subtrair(object));
     }
 
+    private BigDecimal realizarOperacao(Object object, Class annotation) {
+        Class classe = object.getClass();
+        Field[] fields = classe.getDeclaredFields();
+        BigDecimal total = BigDecimal.ZERO;
 
+        for (Field field : fields) {
+            if (field.getAnnotatedType().equals(annotation)) {
+                field.setAccessible(true);
+                try {
+                    Object value = classe.getField(field.getName());
+                    total = total.add((BigDecimal) value);
+                } catch (NoSuchFieldException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+        return total;
+    }
 }
